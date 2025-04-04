3cffa72407c89eadf368f54ea30a2244a3120a4d
Koctr87 <k.pruhlo@gmail.com>
2025-04-04 10:24:09 +0200

    Delete js directory

--- js/phone_publish.js
+++
@@ -1,7 +0,0 @@
-$(window).on('load', function() {
-  $('.phonewrapper .fa').each(function() {
-    if ($(this).hasClass('hidden')) {
-      $(this).next('.phtext').css({'margin-left': '0em'});
-    }
-  });
-});
